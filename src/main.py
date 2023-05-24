1e3e92b14467d55291e81a114c9e33c736dff08b
Abhijith H R <abhijith.hr@ib-systems.org>
2023-05-24 15:40:50 +0200

    added first logic

--- src/main.py
+++ src/main.py
@@ -1,36 +1,67 @@
 from opcua import Client
+import os
+import socket
+import time
+
+target_configs = os.environ.get('TARGET_CONFIGS').split(",")
+
+opc_url = os.environ.get('OPC_URL')
+opc_port = os.environ.get('OPC_PORT')
+
+oisp_url = os.environ.get('OISP_URL')
+oisp_port = os.environ.get('OISP_PORT')
+
+time.sleep(15)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = Client(opc_url + ":" + opc_port)
+
+client.connect()
+s.connect((str(oisp_url), int(oisp_port)))
+root = client.get_root_node()
+
+
+def registerComponent(n, t):
+    try:
+        msgFromClient = '{"n": "' + n + '", "t": "' + t + '"}'
+        s.send(str.encode(msgFromClient))
+        print("Registered component to OISP: " + n + " " + t)
+    except:
+        print("Could not register component to OISP")
+
+
+def fetchOpcData(n, i):
+    try:
+        var = client.get_node(n + ";" + i)
+        print("Fetched data from OPC UA: " + n + " " + i)
+    except:
+        print("Could not fetch data from OPC UA")
+        return None
+    
+    return var.get_value()
+
+
+def sendOispData(n, v):
+    try:
+        msgFromClient = '[{"n": "' + n + '", "v": "' + str(v) + '"}]'
+        s.send(str.encode(msgFromClient))
+        print("Sent data to OISP: " + n + " " + str(v))
+    except:
+        print("Could not send data to OISP")
+
 
 if __name__ == "__main__":
+    for item in target_configs:
+        oisp_n = item.split("|")[2]
+        oisp_t = item.split("|")[3]
+        
+        registerComponent(oisp_n, oisp_t)
 
-    client = Client("opc.tcp://192.168.49.218:62548")
-    # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
-    try:
-        client.connect()
-
-        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
-        root = client.get_root_node()
-        print("Objects node is: ", root)
-
-        # Node objects have methods to read and write node attributes as well as browse or populate address space
-        print("Children of root are: ", root.get_children())
-
-        # get a specific node knowing its node id
-        #var = client.get_node(ua.NodeId(1002, 2))
-        #var = client.get_node("ns=3;i=2002")
-        #print(var)
-        #var.get_data_value() # get value of node as a DataValue object
-        #var.get_value() # get value of node as a python builtin
-        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
-        #var.set_value(3.9) # set node value using implicit data type
-
-        # Now getting a variable node using its browse path
-        # myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
-        # obj = root.get_child(["0:Objects", "2:MyObject"])
-        # print("myvar is: ", myvar)
-        # print("myobj is: ", obj)
-
-        # Stacked myvar access
-        # print("myvar is: ", root.get_children()[0].get_children()[1].get_variables()[0].get_value())
-
-    finally:
-        client.disconnect()
+    while 1:
+        time.sleep(5)
+        for item in target_configs:
+            opc_n = item.split("|")[0]
+            opc_i = item.split("|")[1]
+            oisp_n = item.split("|")[2]
+            opc_value = fetchOpcData(n=opc_n, i=opc_i)
+            sendOispData(n=oisp_n, v=opc_value)
